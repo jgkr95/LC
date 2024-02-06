215824b029d48496bdd17a45ec107f8f19a2be06
jgkr95 <gandhi.jarugu@gmail.com>
2024-02-06 23:14:31 +0530

    Trapping Rain water with O(1) space

---
+++ 42. Trapping Rain Water.java
@@ -0,0 +1,42 @@
+// 42. Trapping Rain Water
+// Solved
+// Hard
+// Topics
+// Companies
+// Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+ 
+
+// Example 1:
+
+
+// Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+// Output: 6
+// Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+// Example 2:
+
+// Input: height = [4,2,0,3,2,5]
+// Output: 9
+ 
+
+ class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int l = 0, r = n - 1;
+        int leftMax = height[0], rightMax = height[n - 1];
+
+        int res = 0;
+        while (l < r) {
+            if (leftMax < rightMax) {
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                res += leftMax - height[l];
+            } else {
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                res += rightMax - height[r];
+            }
+        }
+        return res;
+    }
+}
