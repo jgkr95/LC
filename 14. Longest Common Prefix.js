026897d0fb892ddbd0be3793803ed667cb56cf82
jgkr95 <gandhi.jarugu@gmail.com>
2024-02-08 13:47:09 +0530

    Longest Common Prefix

---
+++ 14. Longest Common Prefix.js
@@ -0,0 +1,33 @@
+// 14. Longest Common Prefix
+// Solved
+// Easy
+// Topics
+// Companies
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  let res = "";
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let s of strs) {
+      if (i == s.length || s[i] != strs[0][i]) return res;
+    }
+    res += strs[0][i];
+  }
+  return res;
+};
