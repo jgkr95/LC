1f1ebd512c9455ab074c3a322e248ced6eaee26c
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2024-02-23 12:46:38 +0530

    Time: 231 ms (8.36%) | Memory: 69.6 MB (5.16%) - LeetSync

--- 221-maximal-square/maximal-square.js
+++ 221-maximal-square/maximal-square.js
@@ -2,28 +2,28 @@
  * @param {character[][]} matrix
  * @return {number}
  */
-var maximalSquare = function (matrix) {
-    let rows = matrix.length, col = matrix[0].length
-    let cache = Array.from({ length: rows }, () => Array(col).fill(-1));
-    console.log(cache)
-    function helper(i, j) {
-        if (i >= rows || j >= col) {
+var maximalSquare = function(matrix) {
+    let rows = matrix.length, col= matrix[0].length
+    let cache = Array.from({ length: rows}, () => Array(col).fill(-1));
+console.log(cache)
+    function helper(i,j){
+        if(i>=rows || j >= col){
             return 0;
         }
-        if (cache[i][j] != -1) return cache[i][j]
-        let down = helper(i + 1, j)
-        let right = helper(i, j + 1)
-        let diag = helper(i + 1, j + 1)
+        if(cache[i][j]=='-1'){
+            let down = helper(i+1,j)
+            let right = helper(i,j+1)
+            let diag = helper(i+1,j+1)
 
-        result = 0;
-        if (matrix[i][j] == '1') {
-            result = 1 + Math.min(down, Math.min(right, diag))
+            cache[i][j]=0;
+            if(matrix[i][j] == '1'){
+                cache[i][j] = 1 + Math.min(down,Math.min(right,diag))
+            }
         }
-        cache[i][j] = result;
-        return result;
+        return cache[i][j];
     }
 
-    helper(0, 0);
+    helper(0,0);
     console.log(cache)
 
     res = Math.max(...cache.flat())
