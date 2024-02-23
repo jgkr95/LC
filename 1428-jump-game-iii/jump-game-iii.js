db1c8a4bf822dae1d58791c996a969ce21f3dc56
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2024-02-23 16:04:40 +0530

    Time: 67 ms (46.09%) | Memory: 58.9 MB (27.39%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.js
+++ 1428-jump-game-iii/jump-game-iii.js
@@ -4,20 +4,20 @@
  * @return {boolean}
  */
 var canReach = function (arr, start) {
-let visited = new Set();
-function dfs(start){
+    let visited = new Set();
+    function dfs(start) {
 
-    if (start < 0 || start > arr.length - 1) return false;
+        if (start < 0 || start > arr.length - 1) return false;
 
-    if(visited.has(start)) return false;
+        if (visited.has(start)) return false;
 
-    if (arr[start] == 0) return true;
+        if (arr[start] == 0) return true;
 
-    visited.add(start)
+        visited.add(start)
 
-    return dfs(start + arr[start]) || dfs( start - arr[start])
-}
+        return dfs(start + arr[start]) || dfs(start - arr[start])
+    }
 
-return dfs(start);
+    return dfs(start);
 
 };
