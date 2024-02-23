8a79bf0e7f0d4bd8d71ece9a7b9adcd80725d0d1
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2024-02-23 12:35:57 +0530

    Time: 203 ms (8.90%) | Memory: 69.6 MB (5.16%) - LeetSync

--- 221-maximal-square/maximal-square.js
+++ 221-maximal-square/maximal-square.js
@@ -1,66 +1,84 @@
-// /**
-//  * @param {character[][]} matrix
-//  * @return {number}
-//  */
-// var maximalSquare = function(matrix) {
-//     let rows = matrix.length, col= matrix[0].length
-//     let cache = Array.from({ length: rows}, () => Array(col).fill(undefined));
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalSquare = function (matrix) {
+    let rows = matrix.length, col = matrix[0].length
+    let cache = Array.from({ length: rows }, () => Array(col).fill(-1));
 
-//     function helper(i,j){
-//         if(i>=rows || j >= col){
+    // function helper(i, j) {
+    //     if (i >= rows || j >= col) {
+    //         return 0;
+    //     }
+    //     if (cache[i][j] != undefined) return cache[i][j]
+    //     // if(!cache[i][j]){
+    //     down = helper(i + 1, j)
+    //     right = helper(i, j + 1)
+    //     diag = helper(i + 1, j + 1)
+
+    //     cache[i][j] = 0;
+    //     if (matrix[i][j] == '1') {
+    //         cache[i][j] = 1 + Math.min(down, Math.min(right, diag))
+    //     }
+    //     // }
+    //     return cache[i][j]
+    // }
+    function helper(i, j) {
+    if (i >= rows || j >= col) {
+        return 0;
+    }
+    if (cache[i][j] !== -1) {
+        return cache[i][j];
+    }
+    let down = helper(i + 1, j);
+    let right = helper(i, j + 1);
+    let diag = helper(i + 1, j + 1);
+    let result = 0;
+    if (matrix[i][j] === '1') {
+        result = 1 + Math.min(down, Math.min(right, diag));
+    }
+    cache[i][j] = result;
+    return result;
+}
+
+    helper(0, 0);
+    console.log(matrix)
+    console.log(cache)
+
+    res = Math.max(...cache.flat())
+    return res * res;
+
+};
+
+// var maximalSquare = function(matrix) {
+//     let rows = matrix.length;
+//     let cols = matrix[0].length;
+//     let cache = Array.from({ length: rows}, () => Array(cols).fill(-1));
+
+//     function helper(i, j) {
+//         if (i >= rows || j >= cols) {
 //             return 0;
 //         }
-//         if(!cache[i][j]){
-//             down = helper(i+1,j)
-//             right = helper(i,j+1)
-//             diag = helper(i+1,j+1)
-
-//             cache[i][j]=0;
-//             if(matrix[i][j] == '1'){
-//                 cache[i][j] = 1 + Math.min(down,Math.min(right,diag))
-//             }
+//         if (cache[i][j] !== -1) {
+//             return cache[i][j];
 //         }
-//         return cache[i][j]
+//         let down = helper(i + 1, j);
+//         let right = helper(i, j + 1);
+//         let diag = helper(i + 1, j + 1);
+//         let result = 0;
+//         if (matrix[i][j] === '1') {
+//             result = 1 + Math.min(down, Math.min(right, diag));
+//         }
+//         cache[i][j] = result;
+//         return result;
 //     }
 
-//     helper(0,0);
-//     console.log(matrix)
-//     console.log(cache)
+//     // for (let i = 0; i < rows; i++) {
+//     //     for (let j = 0; j < cols; j++) {
+//             helper(0, 0);
+//     //     }
+//     // }
 
-//     res = Math.max(...cache.flat())
-//     return res * res;
-
+//     let maxSide = Math.max(...cache.flat());
+//     return maxSide * maxSide;
 // };
-
-var maximalSquare = function(matrix) {
-    let rows = matrix.length;
-    let cols = matrix[0].length;
-    let cache = Array.from({ length: rows}, () => Array(cols).fill(-1));
-
-    function helper(i, j) {
-        if (i >= rows || j >= cols) {
-            return 0;
-        }
-        if (cache[i][j] !== -1) {
-            return cache[i][j];
-        }
-        let down = helper(i + 1, j);
-        let right = helper(i, j + 1);
-        let diag = helper(i + 1, j + 1);
-        let result = 0;
-        if (matrix[i][j] === '1') {
-            result = 1 + Math.min(down, Math.min(right, diag));
-        }
-        cache[i][j] = result;
-        return result;
-    }
-
-    // for (let i = 0; i < rows; i++) {
-    //     for (let j = 0; j < cols; j++) {
-            helper(0, 0);
-    //     }
-    // }
-
-    let maxSide = Math.max(...cache.flat());
-    return maxSide * maxSide;
-};
