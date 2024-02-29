6c3c503e860996003e4c3b872dc0bac31c75b248
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2024-02-29 21:30:19 +0530

    Time: 45 ms (91.83%) | Memory: 48.6 MB (54.90%) - LeetSync

--- 140-word-break-ii/word-break-ii.js
+++ 140-word-break-ii/word-break-ii.js
@@ -3,48 +3,48 @@
  * @param {string[]} wordDict
  * @return {string[]}
  */
-var wordBreak = function(s, wordDict) {
+var wordBreak = function (s, wordDict) {
 
     const wordSet = new Set(wordDict) // Converting to set for faster lookup
 
     const map = {}
 
-    function dfs(start){
+    function dfs(start) {
         // If the result for current index is already calculated, return it
-        if(map[start]){
+        if (map[start]) {
             return map[start]
         }
 
         // If we have reached the end of the string, return an empty array
-        if(start == s.length)
-         return ['']
+        if (start == s.length)
+            return ['']
 
         const sentences = [];
 
         // Try all possible partitions from the current index
-         for(let end=start+1;end<=s.length;end++){
-             // If the substring from start to end is in the wordDict
-             if(wordSet.has(s.substring(start,end))){
+        for (let end = start + 1; end <= s.length; end++) {
+            // If the substring from start to end is in the wordDict
+            if (wordSet.has(s.substring(start, end))) {
 
-                 // Recursively find sentences starting from the next index
-                 const nextSentences = dfs(end);
+                // Recursively find sentences starting from the next index
+                const nextSentences = dfs(end);
 
-                 // Append current word to each sentence and add to results
-                for(let sentence of nextSentences){
-                    if(sentence){
-                        sentences.push(s.substring(start,end)+ " "+ sentence)
-                    }else{
-                        sentences.push(s.substring(start,end))
+                // Append current word to each sentence and add to results
+                for (let sentence of nextSentences) {
+                    if (sentence) {
+                        sentences.push(s.substring(start, end) + " " + sentence)
+                    } else {
+                        sentences.push(s.substring(start, end))
                     }
                 }
-             }
-         }
+            }
+        }
 
-         // Store the result for current index in map
+        // Store the result for current index in map
         map[start] = sentences
-         return sentences;
+        return sentences;
     }
 
     return dfs(0)
-    
+
 };
