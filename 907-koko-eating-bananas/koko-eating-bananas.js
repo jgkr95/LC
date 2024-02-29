73c162aa627c12be73736a0bb0ee6d27f83be917
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2024-02-29 10:06:07 +0530

    Time: 73 ms (41.40%) | Memory: 52 MB (56.66%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    
+    let l=0;
+    let r=Math.max(...piles);
+    let res = r;
+
+    while(l<=r){
+        let mid = Math.ceil((l+r)/2);
+        let hours = 0;
+        for(let i=0;i<piles.length;i++){
+            hours += Math.ceil(piles[i]/mid);
+        }
+        if(hours <= h){
+            res = Math.min(res, mid)
+            r = mid - 1;
+        }else{
+            l = mid + 1;
+        }
+    }
+
+    return res;
+
+};
