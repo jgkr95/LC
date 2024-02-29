2269372bbccda53e357e5cb764c9a02b686991a6
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2024-02-29 21:30:14 +0530

    Time: 40 ms (97.06%) | Memory: 48.3 MB (62.42%) - LeetSync

--- 140-word-break-ii/word-break-ii.js
+++ 140-word-break-ii/word-break-ii.js
@@ -10,17 +10,26 @@ var wordBreak = function(s, wordDict) {
     const map = {}
 
     function dfs(start){
+        // If the result for current index is already calculated, return it
         if(map[start]){
             return map[start]
         }
+
+        // If we have reached the end of the string, return an empty array
         if(start == s.length)
          return ['']
 
         const sentences = [];
+
+        // Try all possible partitions from the current index
          for(let end=start+1;end<=s.length;end++){
+             // If the substring from start to end is in the wordDict
              if(wordSet.has(s.substring(start,end))){
+
+                 // Recursively find sentences starting from the next index
                  const nextSentences = dfs(end);
-                 console.log(start," ",s.substring(start,end)," ","nextSen",nextSentences)
+
+                 // Append current word to each sentence and add to results
                 for(let sentence of nextSentences){
                     if(sentence){
                         sentences.push(s.substring(start,end)+ " "+ sentence)
@@ -30,6 +39,8 @@ var wordBreak = function(s, wordDict) {
                 }
              }
          }
+
+         // Store the result for current index in map
         map[start] = sentences
          return sentences;
     }
