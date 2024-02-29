46e66e7aadd94272a1adfb714c617165f56cce22
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2024-02-29 21:37:07 +0530

    Time: 60 ms (16.34%) | Memory: 48.7 MB (49.02%) - LeetSync

--- 140-word-break-ii/word-break-ii.js
+++ 140-word-break-ii/word-break-ii.js
@@ -27,53 +27,53 @@ var wordBreak = function(s, wordDict) {
     dfs(0, []);
     return res;
 };
-// /**
-//  * @param {string} s
-//  * @param {string[]} wordDict
-//  * @return {string[]}
-//  */
-// var wordBreak = function (s, wordDict) {
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {string[]}
+ */
+var wordBreak = function (s, wordDict) {
 
-//     const wordSet = new Set(wordDict) // Converting to set for faster lookup
+    const wordSet = new Set(wordDict) // Converting to set for faster lookup
 
-//     const map = {}
+    const map = {}
 
-//     function dfs(start) {
-//         // If the result for current index is already calculated, return it
-//         if (map[start]) {
-//             return map[start]
-//         }
+    function dfs(start) {
+        // If the result for current index is already calculated, return it
+        if (map[start]) {
+            return map[start]
+        }
 
-//         // If we have reached the end of the string, return an empty array
-//         if (start == s.length)
-//             return ['']
+        // If we have reached the end of the string, return an empty array
+        if (start == s.length)
+            return ['']
 
-//         const sentences = [];
+        const sentences = [];
 
-//         // Try all possible partitions from the current index
-//         for (let end = start + 1; end <= s.length; end++) {
-//             // If the substring from start to end is in the wordDict
-//             if (wordSet.has(s.substring(start, end))) {
+        // Try all possible partitions from the current index
+        for (let end = start + 1; end <= s.length; end++) {
+            // If the substring from start to end is in the wordDict
+            if (wordSet.has(s.substring(start, end))) {
 
-//                 // Recursively find sentences starting from the next index
-//                 const nextSentences = dfs(end);
+                // Recursively find sentences starting from the next index
+                const nextSentences = dfs(end);
 
-//                 // Append current word to each sentence and add to results
-//                 for (let sentence of nextSentences) {
-//                     if (sentence) {
-//                         sentences.push(s.substring(start, end) + " " + sentence)
-//                     } else {
-//                         sentences.push(s.substring(start, end))
-//                     }
-//                 }
-//             }
-//         }
+                // Append current word to each sentence and add to results
+                for (let sentence of nextSentences) {
+                    if (sentence) {
+                        sentences.push(s.substring(start, end) + " " + sentence)
+                    } else {
+                        sentences.push(s.substring(start, end))
+                    }
+                }
+            }
+        }
 
-//         // Store the result for current index in map
-//         map[start] = sentences
-//         return sentences;
-//     }
+        // Store the result for current index in map
+        map[start] = sentences
+        return sentences;
+    }
 
-//     return dfs(0)
+    return dfs(0)
 
-// };
+};
