adfd0caf712520717c6ee6bd9f0a3421da885303
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2024-02-29 21:36:28 +0530

    Time: 51 ms (60.46%) | Memory: 49.1 MB (14.05%) - LeetSync

--- 140-word-break-ii/word-break-ii.js
+++ 140-word-break-ii/word-break-ii.js
@@ -3,48 +3,77 @@
  * @param {string[]} wordDict
  * @return {string[]}
  */
-var wordBreak = function (s, wordDict) {
-
-    const wordSet = new Set(wordDict) // Converting to set for faster lookup
-
-    const map = {}
-
-    function dfs(start) {
-        // If the result for current index is already calculated, return it
-        if (map[start]) {
-            return map[start]
+var wordBreak = function(s, wordDict) {
+    const res = [];
+    function getWords(index, s) {
+        let words = [];
+        for (let i = index; i < s.length; i++) {
+            words.push(s.substring(index, i + 1));
         }
-
-        // If we have reached the end of the string, return an empty array
-        if (start == s.length)
-            return ['']
-
-        const sentences = [];
-
-        // Try all possible partitions from the current index
-        for (let end = start + 1; end <= s.length; end++) {
-            // If the substring from start to end is in the wordDict
-            if (wordSet.has(s.substring(start, end))) {
-
-                // Recursively find sentences starting from the next index
-                const nextSentences = dfs(end);
-
-                // Append current word to each sentence and add to results
-                for (let sentence of nextSentences) {
-                    if (sentence) {
-                        sentences.push(s.substring(start, end) + " " + sentence)
-                    } else {
-                        sentences.push(s.substring(start, end))
-                    }
-                }
+        return words;
+    }
+    function dfs(startIndex, path) {
+        if (startIndex === s.length) {
+            res.push(path.join(' '));
+        }
+        for (let edge of getWords(startIndex, s)) {
+            if (wordDict.includes(edge)) {
+                path.push(edge);
+                dfs(startIndex + edge.length, path);
+                path.pop();
             }
         }
-
-        // Store the result for current index in map
-        map[start] = sentences
-        return sentences;
     }
-
-    return dfs(0)
-
+    dfs(0, []);
+    return res;
 };
+// /**
+//  * @param {string} s
+//  * @param {string[]} wordDict
+//  * @return {string[]}
+//  */
+// var wordBreak = function (s, wordDict) {
+
+//     const wordSet = new Set(wordDict) // Converting to set for faster lookup
+
+//     const map = {}
+
+//     function dfs(start) {
+//         // If the result for current index is already calculated, return it
+//         if (map[start]) {
+//             return map[start]
+//         }
+
+//         // If we have reached the end of the string, return an empty array
+//         if (start == s.length)
+//             return ['']
+
+//         const sentences = [];
+
+//         // Try all possible partitions from the current index
+//         for (let end = start + 1; end <= s.length; end++) {
+//             // If the substring from start to end is in the wordDict
+//             if (wordSet.has(s.substring(start, end))) {
+
+//                 // Recursively find sentences starting from the next index
+//                 const nextSentences = dfs(end);
+
+//                 // Append current word to each sentence and add to results
+//                 for (let sentence of nextSentences) {
+//                     if (sentence) {
+//                         sentences.push(s.substring(start, end) + " " + sentence)
+//                     } else {
+//                         sentences.push(s.substring(start, end))
+//                     }
+//                 }
+//             }
+//         }
+
+//         // Store the result for current index in map
+//         map[start] = sentences
+//         return sentences;
+//     }
+
+//     return dfs(0)
+
+// };
