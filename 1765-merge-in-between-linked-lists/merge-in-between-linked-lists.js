9575f61eadb891b19c624e6abfbee57592d5ded7
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2024-03-25 12:55:00 +0530

    Time: 156 ms (41.72%) | Memory: 61.7 MB (84.64%) - LeetSync

---
+++ 1765-merge-in-between-linked-lists/merge-in-between-linked-lists.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {number} a
+ * @param {number} b
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeInBetween = function(list1, a, b, list2) {
+
+    let cur = list1;
+    let count = 0;
+
+    while(count < a-1){
+        cur=cur.next;
+        count++;
+    }
+    head = cur;
+    while(count <=b){
+        cur=cur.next; // this will be tail that will be appended to list2 end;
+        count++;
+    }
+    head.next = list2;
+
+    // let list2Cur = list2;
+
+    while(list2.next){
+        list2 = list2.next;
+    }
+    list2.next = cur // Appending tail
+
+return list1
+
+
+    
+};
