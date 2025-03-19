156709a107c1d45806827c78a8bf0695191ceefb
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2025-03-19 09:23:57 +0530

    Time: 9 ms (59.84%) | Memory: 57 MB (51.66%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -2,18 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function (s) {
-    const set = new Set()
-    let l = 0;
-    let res = 0;
-    for (let i = 0; i < s.length; i++) {
-        while (set.has(s[i])) {
-            set.delete(s[l])
-            l++;
+var lengthOfLongestSubstring = function(s) {
+    let charSet = []
+    let l = 0;;
+    let res = 0
+    for(let r=0;r<s.length;r++){
+        while(charSet.includes(s[r])){
+            charSet.shift();
+            l+=1;
         }
-        set.add(s[i])
-        res = Math.max(res, i - l + 1)
+        charSet.push(s[r]);
+        //console.log("charSet", charSet, s.substring(l,r+1))
+        res = Math.max(res,r-l+1)
     }
     return res
-
 };
