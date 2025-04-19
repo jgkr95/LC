0df140c724c7aece9cee4d6922757891ed664629
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2025-04-19 14:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (38.76%) - LeetSync

--- 62-unique-paths/unique-paths.js
+++ 62-unique-paths/unique-paths.js
@@ -6,9 +6,10 @@
 var uniquePaths = function (m, n) {
 
     let rowDp = Array(n).fill(1)
-    for (let i = 1; i < m; i++) {
+    for (let i = 0; i < m; i++) {
         let newRowDp = Array(n).fill(1)
-        for (let j = 1; j < n; j++) {
+        for (let j = 0; j < n; j++) {
+            if(i==0||j==0) continue;
             const up = rowDp[j];
             const left = newRowDp[j - 1]
             newRowDp[j] = up + left;
