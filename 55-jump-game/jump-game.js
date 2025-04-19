d3e7598cc64ccd8432ef42ea7a35caaa5a80ae78
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2025-04-19 14:36:18 +0530

    Time: 0 ms (100.00%) | Memory: 59.1 MB (47.69%) - LeetSync

---
+++ 55-jump-game/jump-game.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let max=0;
+    for(let i=0;i<nums.length;i++){
+        if(max<i) return false;
+        max=Math.max(max,i+nums[i]);
+    }
+    return true;
+    
+};
