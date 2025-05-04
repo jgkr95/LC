f04cf1f6f9f5816e8a426154f61c940e94592f88
jgkr95 <41884413+jgkr95@users.noreply.github.com>
2025-05-04 12:30:32 +0530

    Time: 27 ms (29.21%) | Memory: 67.3 MB (12.52%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
@@ -3,27 +3,35 @@
  * @return {string}
  */
 var minRemoveToMakeValid = function (s) {
-    let cnt = 0
-    let filtered = ''
+    let cnt = 0; // Counter to track unmatched opening parentheses
+    let filtered = [];
+
+    // First pass: remove extra closing parentheses
     for (const char of s) {
-        if (char == '(') {
-            filtered += char
-            cnt++
-        } else if (char == ')' && cnt > 0) {
-            filtered += char
-            cnt--
-        } else if (char != ')') {
-            filtered += char
+        if (char === '(') {
+            filtered.push(char);
+            cnt++; // Track an unmatched opening parenthesis
+        } else if (char === ')' && cnt > 0) {
+            filtered.push(char);
+            cnt--; // Match it with a previous '('
+        } else if (char !== ')') {
+            // Just push any non-parenthesis or characters other than extra ')'
+            filtered.push(char);
+        }
+        // Extra ')' are skipped automatically
+    }
+
+    let res = [];
+
+    // Second pass: remove unmatched opening parentheses from the end
+    for (const char of filtered.reverse()) {
+        if (char === '(' && cnt > 0) {
+            cnt--; // Skip this unmatched '('
+        } else {
+            res.push(char); // Keep valid characters
         }
     }
 
-    let res = ''
-    for (const char of filtered.split('').reverse()) {
-        if (char == '(' && cnt > 0) {
-            cnt--;
-        } else {
-            res += char
-        }
-    }
-    return res.split('').reverse().join('')
+    // Reverse again to restore the original order
+    return res.reverse().join('');
 };
